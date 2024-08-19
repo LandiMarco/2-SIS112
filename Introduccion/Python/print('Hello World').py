424cf607fc945ef0094b32c3ccf19614f78404a4
LandiMarco <marco.landivar@ucb.edu.bo>
2024-08-19 11:55:44 -0400

    Persona.py
    Clase 19/8/2024

---
+++ Introduccion/Python/print('Hello World').py
@@ -0,0 +1,16 @@
+def Hola_Mundo():
+    print("Hello World")
+
+
+def obtener_edad():
+    edad = input("Introduzca su edad: ")
+    print("Su edad es:", edad)
+
+def calcular_edad():
+    anio_nacimiento = int(input("Introduzca su año de nacimiento: "))
+    edad = 2024 - anio_nacimiento
+    print("Su edad es:", edad)
+
+Hola_Mundo()
+obtener_edad()
+calcular_edad()
