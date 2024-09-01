94f04d372a38103a3abae11422b02916e9000c11
LandiMarco <marco.landivar@ucb.edu.bo>
2024-09-01 15:36:48 -0400

    Persona.py
    He modificado el codigo para que este lo mas parecido al codigo hecho en java script.

--- POO/Py/Persona.py
+++ POO/Py/Persona.py
@@ -1,23 +1,81 @@
 class Persona:
-    def __init__(self, nombre, edad, carrera):
+    def __init__(self, nombre, edad, carrera, universidad):
         self.nombre = nombre
         self.edad = edad
         self.carrera = carrera
+        self.universidad = universidad
 
     def saludar(self):
-        print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años")
+        return f"Hola, soy {self.nombre}"
 
-    def cumpleaños(self):
-        self.edad += 1
-        print(f"Feliz cumpleaños! Ahora tengo {self.edad} años.")
+    def cumpleanios(self):
+        return f"Yo tengo {self.edad} años."
 
     def estudiar(self):
-        print(f"Estoy estudiando {self.carrera}.")
+        return f"Estoy estudiando {self.carrera}"
 
-# Crear una instancia de la clase persona
-persona1 = Persona("Juan", 20, "Medicina")
-persona2 = Persona("Samuel", 18, "Ingenieria industrial")
-#Llamar a los metodos
-persona1.saludar()
-persona2.saludar()
-#persona1.estudiar()
+    def mostrarUniversidad(self):
+        return f"Estoy estudiando en la {self.universidad}"
+
+    def modificarNombre(self):
+        while True:
+            nuevo_nombre = input("Ingresa tu nombre: ")
+            if self.validarTexto(nuevo_nombre):
+                self.nombre = nuevo_nombre
+                break
+            else:
+                print("Caracter no valido, ingrese solamente letras.")
+        return self.saludar()
+
+    def modificarEdad(self):
+        while True:
+            try:
+                nueva_edad = int(input("Ingresa tu edad: "))
+                if 0 <= nueva_edad <= 118:
+                    self.edad = nueva_edad
+                    break
+                else:
+                    print("Edad no valida, ingrese un número entre 0 y 118.")
+            except ValueError:
+                print("Por favor ingrese un número válido.")
+        return f"¡Felicidades! Tienes {self.edad} años."
+
+    def modificarCarrera(self):
+        while True:
+            nueva_carrera = input("Ingresa tu carrera: ")
+            if self.validarTexto(nueva_carrera):
+                self.carrera = nueva_carrera
+                break
+            else:
+                print("Caracter no valido, ingrese solamente letras.")
+        return self.estudiar()
+
+    def modificarUniversidad(self):
+        nueva_universidad = input("Ingresa tu universidad: ")
+        self.universidad = nueva_universidad
+        return f"Estas estudiando en la {self.universidad}"
+
+    def validarTexto(self, texto):
+        return texto.replace(" ", "").isalpha()
+
+    def eliminarNombre(self):
+        self.nombre = None
+
+    def eliminarEdad(self):
+        self.edad = None
+
+    def eliminarCarrera(self):
+        self.carrera = None
+
+    def eliminarUniversidad(self):
+        self.universidad = None
+
+# Crear una instancia de la clase Persona
+persona = Persona('Marco', 19, 'Ingenieria industrial', 'Universidad Catolica Boliviana')
+
+# Ejemplos de uso de los métodos
+print(persona.saludar())
+print(persona.modificarNombre())
+print(persona.modificarEdad())
+print(persona.modificarCarrera())
+print(persona.modificarUniversidad())
