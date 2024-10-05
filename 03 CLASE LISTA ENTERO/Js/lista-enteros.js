8ac81fe84f20d390d6a95696557c16a17908ccb6
LandiMarco <marco.landivar@ucb.edu.bo>
2024-10-05 14:44:49 -0400

    Clase 5/10

--- 03 CLASE LISTA ENTERO/Js/lista-enteros.js
+++ 03 CLASE LISTA ENTERO/Js/lista-enteros.js
@@ -23,3 +23,72 @@ class ListaEnteros{
         }
     }
 
+const listaEnteros = new ListaEnteros();
+listaEnteros.agregar(21);
+listaEnteros.agregar(12);
+listaEnteros.agregar(15);
+listaEnteros.agregar(30);
+console.log(listaEnteros.lista);
+
+var a = listaEnteros.lista[0];
+console.log(a);
+
+var b = listaEnteros.lista[1];
+console.log(b);
+
+var c = listaEnteros.lista[2];
+console.log(c);
+
+var d = listaEnteros.lista[3];
+console.log(d);
+
+
+//RETORNAR EL ELEMENTO MENOR DE LA LISTA
+/*
+var datoMenor = -1;
+
+var a = listaEnteros.lista[0];
+if(a > datoMenor){
+  datoMenor = a;
+}
+console.log("Dato menor: " + datoMenor);
+
+//------------------------------------------------
+var b = listaEnteros.lista[1];
+if(a > b){
+  datoMenor = b
+}
+console.log("Dato menor: " + datoMenor)
+
+//------------------------------------
+var c = listaEnteros.lista[2];
+if(b > c){
+  datoMenor = c
+}
+console.log("Dato menor: " + datoMenor)
+//------------------------------------------
+var d = listaEnteros.lista[3];
+if(c > d){
+  datoMenor = d
+}
+console.log("Dato menor: " + datoMenor)
+
+*/
+//----------------------------
+var lista = listaEnteros.lista;
+var datoMenor = lista[0]; 
+var datoMayor = lista[0]; 
+for (var i = 1; i < lista.length; i++) { 
+  var valorActual = lista[i];
+  
+  if (valorActual < datoMenor) {
+    datoMenor = valorActual;
+  } 
+  if (valorActual > datoMayor) {
+    datoMayor = valorActual;
+  }
+}
+
+console.log("Dato menor: " + datoMenor);
+console.log("Dato mayor: " + datoMayor);
+
